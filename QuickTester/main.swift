ff74e2da860277ee9ba8bd2149bafa81cf37675e
Christopher Ray <vde435@gmail.com>
2021-01-08 22:03:22 -0600

    Update main.swift

--- QuickTester/main.swift
+++ QuickTester/main.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
-var thirdGreeting = "third!"
-print("Hello, World!")
-print("I second that greeting!")
-print(thirdGreeting)
+var greeting1 = "Hello world!"
+var greeting2 = "I second that greeting!"
+var greeting3 = "Third!"
+
+print(greeting1 + " " + greeting2 + " " + greeting3)
+
