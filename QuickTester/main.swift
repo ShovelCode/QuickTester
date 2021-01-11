1d6b520b1e386407faff5438426a0abd5d5642c6
Christopher Ray <vde435@gmail.com>
2021-01-11 10:54:19 -0600

    update

--- QuickTester/main.swift
+++ QuickTester/main.swift
@@ -10,6 +10,10 @@ import Foundation
 var greeting1 = "Hello world!"
 var greeting2 = "I second that greeting!"
 var greeting3 = "Third!"
+var isLoggedIn = true
 
 print(greeting1 + " " + greeting2 + " " + greeting3)
 
+if isLoggedIn {
+    print("Logged in.")
+}
