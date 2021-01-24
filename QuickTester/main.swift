90ede3a9209d4b587b5d56b7700e56722ccf1f7e
Christopher Ray <vde435@gmail.com>
2021-01-24 14:59:40 -0600

    update

--- QuickTester/main.swift
+++ QuickTester/main.swift
@@ -44,3 +44,18 @@ switch secretIdentity {
 }
 
 print(superheroName)
+
+//new function
+var episode = 8
+var villain: String
+
+// Write your code below 🦹
+
+switch episode{
+ case 1...3 : villain = "Emperor Palpatine"
+ case 4...6 : villain = "Darth Vader"
+ case 7...9 : villain = "Kylo Ren"
+  default: villain = ""
+}
+
+print(villain)
